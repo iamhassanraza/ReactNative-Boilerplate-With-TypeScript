f99c0195101a066246de01c1482c768aaece55d3
Your Name <you@example.com>
2022-03-30 16:27:56 +0500

    Splash Screen Added

--- ios/RNBP/AppDelegate.m
+++ ios/RNBP/AppDelegate.m
@@ -1,8 +1,10 @@
 #import "AppDelegate.h"
+#import "RNSplashScreen.h"
 
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+
 
 #ifdef FB_SONARKIT_ENABLED
 #import <FlipperKit/FlipperClient.h>
@@ -47,6 +49,7 @@ static void InitializeFlipper(UIApplication *application) {
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+  [RNSplashScreen show];
   return YES;
 }
 
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, {useEffect} from 'react';
 import {store, persistor} from 'store/createStore';
 import {Provider} from 'react-redux';
 import {PersistGate} from 'redux-persist/lib/integration/react';
 import Navigation from 'navigation';
 import {RootSiblingParent} from 'react-native-root-siblings';
+import SplashScreen from 'react-native-splash-screen';
 
 const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <RootSiblingParent>
       <Provider store={store}>
