53e44ed547dd8915ad96e400fdf38e77e281619d
macbook <macbook@MacBook-Pro.local>
2023-05-13 01:27:14 +0500

    multi-language-support

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
 import 'react-native-gesture-handler';
-import React, {useEffect} from 'react';
-import {store, persistor} from 'store/createStore';
-import {Provider} from 'react-redux';
-import {PersistGate} from 'redux-persist/lib/integration/react';
+import 'translations/index'
+import { useTranslation } from 'react-i18next';
+import React, { useEffect, useState } from 'react';
+import { store, persistor } from 'store/createStore';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/lib/integration/react';
 import Navigation from 'navigation';
-import {RootSiblingParent} from 'react-native-root-siblings';
+import { RootSiblingParent } from 'react-native-root-siblings';
 import SplashScreen from 'react-native-splash-screen';
 
 const App = () => {
+
+  // const { t, i18n, ready } = useTranslation();
+  // const [value, setValue] = useState('en');
+  // const [items, setItems] = useState([
+  //   {label: 'English', value: 'en'},
+  //   {label: 'Chinese', value: 'zh'},
+  // ]);
+  // useEffect(() => {
+  //   i18n.changeLanguage(value);
+  // }, [value]);
+
+
   useEffect(() => {
     SplashScreen.hide();
   }, []);
